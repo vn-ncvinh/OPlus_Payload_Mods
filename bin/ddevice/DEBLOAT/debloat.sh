6e9a86b6b6b86bf6a6e4b4155a396b9ad53e49f0
vn-ncvinh <vn.ncvinh@gmail.com>
2026-09-02 23:00:13 +0700

    Constrain debloat package removal

--- bin/ddevice/DEBLOAT/debloat.sh
+++ bin/ddevice/DEBLOAT/debloat.sh
@@ -22,20 +22,22 @@ if [[ -d "$IMAGES_DIR/my_product" ]]; then
     while IFS= read -r -d '' path; do
         remove_path "$path"
     done < <(find "$IMAGES_DIR/my_product/etc" -maxdepth 1 -name 'auto-install*' -print0 2>/dev/null || true)
-    remove_path "$IMAGES_DIR/my_product/app/Updater"
-    remove_path "$IMAGES_DIR/my_product/etc/permissions/cn.google.services.xml"
 fi
 
-while IFS= read -r -d '' app_dir; do
-    app_name="${app_dir##*/}"
-    if [[ -n "${debloat_apps[$app_name]+x}" ]]; then
-        remove_path "$app_dir"
-    fi
-done < <(
-    for part in system_ext my_product my_stock; do
-        [[ -d "$IMAGES_DIR/$part" ]] || continue
-        find "$IMAGES_DIR/$part" -depth -type d -print0
-    done
-)
+for part in system_ext my_product my_stock; do
+    [[ -d "$IMAGES_DIR/$part" ]] || continue
+    while IFS= read -r -d '' app_root; do
+        while IFS= read -r -d '' app_dir; do
+            app_name="${app_dir##*/}"
+            if [[ -n "${debloat_apps[$app_name]+x}" ]]; then
+                remove_path "$app_dir"
+            fi
+        done < <(find "$app_root" -mindepth 1 -maxdepth 1 -type d -print0)
+    done < <(
+        find "$IMAGES_DIR/$part" -type d \
+            \( -name app -o -name priv-app -o -name del-app -o -name del-app-pre \) \
+            -print0
+    )
+done
 
 mods "Debloat completed"
